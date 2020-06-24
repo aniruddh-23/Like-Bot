d62649cf821e23d484f3284ded7c0b84d0c3cef8
KR Chandra <anicd01@gmail.com>
2020-06-24 14:04:56 +0530

    init

---
+++ link opner.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 27 13:40:50 2020
+
+@author: Aayu
+"""
+import webbrowser
+
+def extract(link_list):
+    ll=[]
+    for l in link_list:
+        ll.append(l.split(' ')[1])
+    return ll
+
+    
+links = open("ig_links.txt","r")
+ll=[]
+for i in links:
+    ll.append(i)
+ll=extract(ll)
+c=0
+for i in ll:
+    c+=1
+    if c>50:
+        c=0
+        input()
+    webbrowser.open(i)
